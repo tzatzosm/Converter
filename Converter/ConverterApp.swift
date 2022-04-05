64dc88175b920b3ad51e26e920c70edd51f80cd8
Marsel Tzatzo <tmarselg@gmail.com>
2022-04-05 17:24:22 +0300

    Initial Commit

---
+++ Converter/ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConverterApp.swift
+//  Converter
+//
+//  Created by Marsel Tzatzo on 5/4/22.
+//
+
+import SwiftUI
+
+@main
+struct ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
